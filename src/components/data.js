c0190a4810f48d0b365fca722b18d2c7dfd7b685
ManideepVaddepalli <manideep15168@gmail.com>
2023-10-31 19:16:26 +0530

    one

--- src/components/data.js
+++ src/components/data.js
@@ -3,6 +3,7 @@ import spaceImage from "@/images/spaceExploration.png";
 import teamImage from "@/images/teamApp.png";
 import todoImage from "@/images/todoApp.png";
 import adviceImage from "@/images/adviceGeneratorApp.png";
+import agecalImage from "@/images/ageCaliculatorApp.png";
 
 export const data = [
   {
@@ -17,6 +18,7 @@ export const data = [
       "ExpressJS",
       "JWT-token",
       "Bcrypt",
+      "Responsive",
     ],
     codeLink: "https://github.com/ManideepVaddepalli/shopCo-frontend",
     liveDemo: "https://shop-co-frontend.vercel.app/",
@@ -26,7 +28,7 @@ export const data = [
     id: 2,
     title: "Space-Exploration 🪐",
     desc: "The Space-Exploration is an online website. Which was developed according to the Figma designs provided by the FrontEnd-Mentor Website. It has many conditionally rendered parts.",
-    tags: ["Forntend", "NextJS", "React"],
+    tags: ["Forntend", "NextJS", "React", "Responsive"],
     codeLink: "https://github.com/ManideepVaddepalli/space-exploration-next-js",
     liveDemo: "https://space-exploration-next-js.vercel.app/",
     image: spaceImage,
@@ -35,7 +37,7 @@ export const data = [
     id: 3,
     title: "Team App ",
     desc: "Team App is a business Blog website with dynamically rendered pages. The design was developed by referencing a Figma community design.",
-    tags: ["Forntend", "NextJS", "React"],
+    tags: ["Forntend", "NextJS", "React", "Responsive"],
     codeLink: "https://github.com/ManideepVaddepalli/TeamApp-BlogPage-NextJS",
     liveDemo: "https://teamappnextjs.vercel.app/",
     image: teamImage,
@@ -44,7 +46,7 @@ export const data = [
     id: 4,
     title: "TO-DO App 📝",
     desc: "To-do app is a frontend app which is used to manage the task lists. This app provides interface of Dark and Light Mode, Filter option for Completed and Incomplete taskes, Shows the total count of tasks, Adding new tasks etc..",
-    tags: ["Forntend", "Vite + React"],
+    tags: ["Forntend", "Vite + React", "Responsive", "D/L modes"],
     codeLink: "https://github.com/ManideepVaddepalli/todo-app-react-vite",
     liveDemo: "https://todo-app-react-vite-kappa.vercel.app/",
     image: todoImage,
@@ -53,11 +55,20 @@ export const data = [
     id: 5,
     title: "Advice Generator App",
     desc: "Developed by referencing the design of Frontend-Mentor. The app generates a new advice for every click of a button.",
-    tags: ["Forntend", "Vite + React", "API Fetching"],
+    tags: ["Forntend", "Vite + React", "API Fetching", "Responsive"],
     codeLink:
       "https://github.com/ManideepVaddepalli/Advice-Generator-App-Using-API",
     liveDemo:
       "https://manideepvaddepalli.github.io/Advice-Generator-App-Using-API",
     image: adviceImage,
+  },
+  {
+    id: 6,
+    title: "Age Caliculator App 🖩",
+    desc: "Developed by referencing the design of Frontend-Mentor. The app can be used to caliculate the age.",
+    tags: ["Forntend", "React", "DOM Manipulation"],
+    codeLink: "https://github.com/ManideepVaddepalli/Age-Calculator-App",
+    liveDemo: "https://manideepvaddepalli.github.io/Age-Calculator-App/",
+    image: agecalImage,
   },
 ];
